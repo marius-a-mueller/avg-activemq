3550cf7c975c99b33a4bdb7e2b61b3a63f13e439
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-04-11 16:26:25 +0200

    Created simple ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { useState } from "react";
 
 export default function Page() {
+  const [message, setMessage] = useState("");
+
   return (
-    <div>
-      <Input />
-      <Button />
+    <div className="flex justify-center items-center">
+      <Card className="h-[600px] w-[800px] mt-20">
+        <CardHeader>
+          <CardTitle> Prepare your message</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2">
+          <Label>Message</Label>
+          <Input value={message} onChange={(e: any) => setMessage(e)} />
+          <Button>Send Message</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
