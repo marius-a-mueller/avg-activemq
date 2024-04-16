69b50696a04d85db6e2879dcae8cd33c7bc54950
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-04-16 19:34:33 +0200

    Formatted main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
-import { test } from '@/lib/broker';
 import { Label } from '@radix-ui/react-label';
 import { useState } from 'react';
 
@@ -17,9 +16,15 @@ export default function Page() {
           <CardTitle>Stock Message Broker</CardTitle>
         </CardHeader>
         <CardContent className="flex flex-col gap-2">
-          <Button onClick={() => fetch('/api/stockholder')}>Start Stockholder</Button>
-          <Button onClick={() => fetch('/api/stockmarket')}>Start Stockmarket</Button>
-          <Button onClick={() => fetch('/api/stockmarketservice')}>Start StockMarketService</Button>
+          <Button onClick={() => fetch('/api/stockholder')}>
+            Start Stockholder
+          </Button>
+          <Button onClick={() => fetch('/api/stockmarket')}>
+            Start Stockmarket
+          </Button>
+          <Button onClick={() => fetch('/api/stockmarketservice')}>
+            Start StockMarketService
+          </Button>
         </CardContent>
       </Card>
     </div>
